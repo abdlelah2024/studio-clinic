1fdd9b8ad9dc517374c4bc0a845bf21b977152fa
asd asd <asd19082@gmail.com>
2025-07-09 21:10:12 +0000

    Next

--- src/components/patients/add-patient-dialog.tsx
+++ src/components/patients/add-patient-dialog.tsx
@@ -12,7 +12,6 @@ import {
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import type { Patient } from "@/lib/types"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
